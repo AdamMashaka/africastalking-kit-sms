e78e469f47c2267e587a724b06fd63d043dab173
AdamMashaka <mashakaadam123@gmail.com>
2026-05-29 23:15:29 +0300

    yes

--- afri_auth/providers/africastalking.py
+++ afri_auth/providers/africastalking.py
@@ -1,17 +1,29 @@
+from afri_auth.config import AFRICASTALKING_USERNAME
 import africastalking
-from afri_auth.config import (
-    AFRICASTALKING_USERNAME,
-    AFRICASTALKING_API_KEY
+
+# from afri_auth.config import (
+#     AFRICASTALKING_USERNAME,
+#     AFRICASTALKING_API_KEY
 )
 
-africastalking.initialize(
-    AFRICASTALKING_USERNAME,
-    AFRICASTALKING_API_KEY
-)
+def get_sms_client():
 
+    africastalking.initialize(
+        AFRICASTALKING_USERNAME,
+        AFRICASTALKING_API_KEY
+    )
+
+    return africastalking.SMS
 sms = africastalking.SMS
 
 
-async def send_sms(phone: str, message: str):
-    response = sms.send(message, [phone])
+async def send_sms(phone, message):
+
+    sms = get_sms_client()
+
+    response = sms.send(
+        message,
+        [phone]
+    )
+
     return response
